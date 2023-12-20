439d37c48abdbd45dfbe0f897bf09581110e9462
Abdullah Roshan <85522748+roshanabdullah@users.noreply.github.com>
2023-12-20 14:35:20 +0500

    Create climbing-stairs.py

---
+++ climbing-stairs.py
@@ -0,0 +1,16 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        cache = {}
+
+        def helper(n):
+            if n == 0 or n == 1:
+                return 1
+            if n in cache:
+                return cache[n]
+
+            else:
+                steps = helper(n - 1) + helper(n - 2)
+                cache[n] = steps
+                return cache[n]
+
+        return helper(n)
