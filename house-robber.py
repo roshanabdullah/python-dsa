3a8a6ee0a890d6fd265814fd1d8274a44d8dfed2
Abdullah Roshan <85522748+roshanabdullah@users.noreply.github.com>
2023-12-20 14:37:32 +0500

    Create house-robber.py

---
+++ house-robber.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy = prices[0]
+        maxProfit = 0
+
+        for sell in prices[1:]:
+            if sell > buy:
+                maxProfit = max(maxProfit, sell - buy)
+            else:
+                buy = sell
+        return maxProfit
